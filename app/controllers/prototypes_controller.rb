92c1954bd54c213674e5c9ec6c8be2e5dcb4ce0a
momotad <momota.d@outlook.jp>
2023-08-04 23:05:06 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, only: [:edit]
+  before_action :set_prototype, only: [:show, :edit, :update]
+  
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.valid?
+      @prototype.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    # @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    # @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    # @prototype = Prototype.find(params[:id])
+    @prototype.assign_attributes(prototype_params)
+    if @prototype.valid?
+      @prototype.save
+      redirect_to '/'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to '/'
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+end
